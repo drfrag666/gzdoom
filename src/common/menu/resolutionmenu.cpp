92a2b11337e5c44329ef22cd8e9767cfcc812980
CandiceJoy <candice@candicejoy.com>
2023-01-01 22:51:37 -0500

    Fixed custom height and width not saving.
    Signed-off-by: CandiceJoy <candice@candicejoy.com>
    
    # Conflicts:
    #	src/common/menu/resolutionmenu.cpp

--- src/common/menu/resolutionmenu.cpp
+++ src/common/menu/resolutionmenu.cpp
@@ -94,8 +94,8 @@ CCMD (menu_resolution_commit_changes)
 			menu_resolution_custom_width = 640;
 			menu_resolution_custom_height = 400;
 		}
-		vid_scale_customwidth = menu_resolution_custom_width;
-		vid_scale_customheight = menu_resolution_custom_height;
+		vid_scale_customwidth = *menu_resolution_custom_width;
+		vid_scale_customheight = *menu_resolution_custom_height;
 		vid_scale_custompixelaspect = 1.0;
 	}
 }
